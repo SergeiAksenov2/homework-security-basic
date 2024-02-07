dc3d00dce9bf0b78a319e2d103ca772d186c85d4
Sergei Aksenov <s.aksenov@haulmont.com>
2024-02-07 18:56:54 +0400

    Added "Editing employees of own department" role

---
+++ src/main/java/com/sample/employees/security/UserRole.java
@@ -0,0 +1,34 @@
+package com.sample.employees.security;
+
+import com.sample.employees.entity.Department;
+import com.sample.employees.entity.Employee;
+import com.sample.employees.entity.User;
+import io.jmix.security.model.EntityAttributePolicyAction;
+import io.jmix.security.model.EntityPolicyAction;
+import io.jmix.security.role.annotation.EntityAttributePolicy;
+import io.jmix.security.role.annotation.EntityPolicy;
+import io.jmix.security.role.annotation.ResourceRole;
+import io.jmix.securityui.role.annotation.MenuPolicy;
+import io.jmix.securityui.role.annotation.ScreenPolicy;
+
+@ResourceRole(name = "UserRole", code = UserRole.CODE)
+public interface UserRole {
+    String CODE = "user-role";
+
+    @MenuPolicy(menuIds = {"Department.browse", "Employee.browse", "User.browse"})
+    @ScreenPolicy(screenIds = {"Department.browse", "Employee.browse", "User.browse", "Department.edit", "Employee.edit", "User.edit"})
+    void screens();
+
+    @EntityAttributePolicy(entityClass = Employee.class, attributes = {"*"}, action = EntityAttributePolicyAction.MODIFY)
+//    @EntityAttributePolicy(entityClass = Employee.class, attributes = {"firstName", "lastName", "email", "birthDate", "department", "id", "*"}, action = EntityAttributePolicyAction.MODIFY)
+    @EntityPolicy(entityClass = Employee.class, actions = {EntityPolicyAction.READ, EntityPolicyAction.UPDATE, EntityPolicyAction.DELETE})
+    void employee();
+
+    @EntityAttributePolicy(entityClass = Department.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
+    @EntityPolicy(entityClass = Department.class, actions = EntityPolicyAction.READ)
+    void department();
+
+    @EntityAttributePolicy(entityClass = User.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
+    @EntityPolicy(entityClass = User.class, actions = EntityPolicyAction.READ)
+    void user();
+}
